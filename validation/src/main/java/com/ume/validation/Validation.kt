c79d6abecd5f16711799cb74e00b0d5628d42175
prodigybest <umeugwadabeluchi@gmail.com>
2022-11-06 16:26:20 -0500

    update validation module

--- validation/src/main/java/com/ume/validation/Validation.kt
+++ validation/src/main/java/com/ume/validation/Validation.kt
@@ -1,6 +1,5 @@
 package com.ume.validation
 
-import android.view.View
 import java.util.*
 
 class Validation {
@@ -16,20 +15,10 @@ class Validation {
         return valid
     }
 
-    @Suppress("UNCHECKED_CAST")
-    fun getValidator(view: View): List<Validator> {
-        val validators = view.getTag(R.id.validator) as ArrayList<Validator>?
-        return if (validators == null) emptyList() else Collections.unmodifiableList(validators)
-    }
+    fun getValidators(): List<Validator> = Collections.unmodifiableList(validators)
 
     @Suppress("UNCHECKED_CAST")
-    fun attachValidator(validator: Validator, view: View) {
-        var list = view.getTag(R.id.validator) as ArrayList<Validator>?
-        if (list == null) {
-            list = ArrayList()
-            view.setTag(R.id.validator, list)
-        }
-        list.add(validator)
+    fun addValidator(validator: Validator) {
         validators.add(validator)
     }
 
